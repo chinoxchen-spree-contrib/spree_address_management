d871154caf737f8f012cdbffb7032122b090f0cc
rmartinezapp <robert.martinez.app@gmail.com>
2023-03-08 07:44:59 -0300

    add scopes to address

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,6 +1,8 @@
 module Spree
   module AddressDecorator
     def self.prepended(base)
+      base.scope :show_in_frontend, -> { where(hide_in_frontend: false) }
+      base.scope :hide_in_frontend, -> { where(hide_in_frontend: true) }
       base.has_many :store_addresses, class_name: 'Spree::StoreAddress'
       base.has_many :stores, through: :store_addresses, class_name: 'Spree::Store'
 
